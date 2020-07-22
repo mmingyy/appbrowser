762d161223fe0d7e091033b8ac16aace4691a911
min <studyg00gle@gmail.com>
2020-07-22 17:49:58 +0900

    open webpage with button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-
+import {InAppBrowser, InAppBrowserOptions} from '@ionic-native/in-app-browser/ngx'
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private iab: InAppBrowser,) {}
+  
+  openWebpage(){
+    const url = 'http://dolearn.co.kr/';
 
-  constructor() {}
-
+    const options: InAppBrowserOptions = {
+      zoom: 'yes'
+    }
+    //opening a URL and returning an InAppBrowserObject
+    const browser = this.iab.create(url,'_self',options);
+  }
 }
