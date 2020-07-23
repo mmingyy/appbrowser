c81e61b1bd6e45a39f864cd423ac9f784b31db35
min <studyg00gle@gmail.com>
2020-07-23 17:53:11 +0900

    allow url in webview(API 28 +)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,31 @@
 import { Component } from '@angular/core';
-import {InAppBrowser, InAppBrowserOptions} from '@ionic-native/in-app-browser/ngx'
+import {InAppBrowser, InAppBrowserOptions} from '@ionic-native/in-app-browser/ngx';
+import {Platform} from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(private iab: InAppBrowser,) {}
+  // constructor(private iab: InAppBrowser,) {
+  // }
+
+  subscribe:any;
+  constructor(private iab: InAppBrowser,public platform:Platform) {
+    this.subscribe = this.platform.backButton.subscribeWithPriority(666666,()=>{
+      if(this.constructor.name == "HomePage"){
+        if(window.confirm("exit app?")){
+          navigator["app"].exitapp();
+        }
+      }
+    });
+  }
   
   openWebpage(){
     const url = 'http://dolearn.co.kr/';
 
     const options: InAppBrowserOptions = {
-      zoom: 'yes'
+      zoom: 'no'
     }
     //opening a URL and returning an InAppBrowserObject
     const browser = this.iab.create(url,'_self',options);
