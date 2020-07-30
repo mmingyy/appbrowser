ee8002fa6db3f8e6fa1e044d613f129a3b2630e3
min <studyg00gle@gmail.com>
2020-07-30 14:15:03 +0900

    start with REAL WEB SITE(not with button)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {InAppBrowser, InAppBrowserOptions} from '@ionic-native/in-app-browser/ngx';
-import {Platform} from '@ionic/angular';
+import {Platform, ToastController, NavController} from '@ionic/angular';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -9,26 +10,47 @@ import {Platform} from '@ionic/angular';
 export class HomePage {
   // constructor(private iab: InAppBrowser,) {
   // }
+  constructor(
+    public navCtrl: NavController,
+    private iab: InAppBrowser,
+    private platform: Platform,
+    private toastCtrl: ToastController,
+    // private splashScreen: SplashScreen,
+    // private statusBar: StatusBar
 
-  subscribe:any;
-  constructor(private iab: InAppBrowser,public platform:Platform) {
-    this.subscribe = this.platform.backButton.subscribeWithPriority(666666,()=>{
-      if(this.constructor.name == "HomePage"){
-        if(window.confirm("exit app?")){
-          navigator["app"].exitapp();
-        }
+  ) {
+      // in-app-browser 생성
+      const options: InAppBrowserOptions = {
+        zoom: 'no',
+        location: 'no',
+        toolbar: 'no',
+        hideurlbar: 'yes'
       }
-    });
-  }
-  
-  openWebpage(){
-    const url = 'http://dolearn.co.kr/';
 
-    const options: InAppBrowserOptions = {
-      zoom: 'no'
-    }
-    //opening a URL and returning an InAppBrowserObject
-     const browser = this.iab.create(url,'_self',options);
-    //const browser = this.iab.create(url,'_system',options);
+      // const browser = this.iab.create('http://dolearn.co.kr', '_system', 'location=no, hideurlbar=yes, toolbar=no');
+      const browser = this.iab.create('http://dolearn.co.kr', '_self', 'location=no, hideurlbar=yes, toolbar=no');
   }
+
+
+  // subscribe:any;
+  // constructor(private iab: InAppBrowser,public platform:Platform) {
+  //   this.subscribe = this.platform.backButton.subscribeWithPriority(666666,()=>{
+  //     if(this.constructor.name == "HomePage"){
+  //       if(window.confirm("exit app?")){
+  //         navigator["app"].exitApp(); //.exitapp()
+  //       }
+  //     }
+  //   });
+  // }
+  
+  // openWebpage(){
+  //   const url = 'http://dolearn.co.kr/';
+
+  //   const options: InAppBrowserOptions = {
+  //     zoom: 'no'
+  //   }
+  //   //opening a URL and returning an InAppBrowserObject
+  //    const browser = this.iab.create(url,'_self',options);
+  //   //const browser = this.iab.create(url,'_system',options);
+  // }
 }
